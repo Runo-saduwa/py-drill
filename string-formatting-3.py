91431b88af1da8c0bd927bda02f1df1b37625513
runo saduwa <orunosaduwa@gmail.com>
2025-01-15 20:50:43 +0100

    updates showed up

--- string-formatting-3.py
+++ string-formatting-3.py
@@ -2,6 +2,8 @@
 name = 'runo'
 intro = f'hi my name is {name}' # just like template literals in JS
 
+test = f'my name is {name}'
+
 intro_2 = 'my elder sisters name is {}';
 
 intro_3 = 'my two sisters names are {} and {}'
